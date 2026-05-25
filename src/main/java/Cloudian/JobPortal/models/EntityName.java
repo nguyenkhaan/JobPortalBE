f5df8fea082b560f75ee60f774c39a1f48d84e66
cloudian <nguyenkhaan2006@gmail.com>
2026-05-25 09:56:08 +0700

    fix: update database entity for audit log

---
+++ src/main/java/Cloudian/JobPortal/models/EntityName.java
@@ -0,0 +1,17 @@
+package Cloudian.JobPortal.models;
+
+public enum EntityName {
+    User,
+    Resume,
+    EmploymentProfile,
+    Industry,
+    JobApplication,
+    JobIndustry,
+    JobPost,
+    JobSeekerProfile,
+    Payment,
+    OAuth,
+    Social,
+    UserRole,
+    Token
+}
