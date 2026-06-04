7ad97b18739bd9ed7839fc749fe18974e265ec16
cloudian <nguyenkhaan2006@gmail.com>
2026-06-04 21:30:45 +0700

    fix: tre con sa mac truyen tai nhau bai dong dao, xuogn rong dom la dom hoa, nuoc dong day tren cao nguyen da. La ngay hoang de tro ve nha

--- src/main/java/Cloudian/JobPortal/modules/employer/dto/CreateEmployerProfileRequest.java
+++ src/main/java/Cloudian/JobPortal/modules/employer/dto/CreateEmployerProfileRequest.java
@@ -5,13 +5,11 @@ import com.fasterxml.jackson.annotation.JsonSetter;
 import com.fasterxml.jackson.annotation.Nulls;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
-@Data
+@Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
