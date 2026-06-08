1ccc199fd564a18fdd8be8e73ab01f54f3d0d69e
WinPear <leminhthang24012006@gmail.com>
2026-06-08 20:52:31 +0700

    nothing

--- src/main/java/Cloudian/JobPortal/modules/auth/AuthService.java
+++ src/main/java/Cloudian/JobPortal/modules/auth/AuthService.java
@@ -283,6 +283,7 @@ public class AuthService
 
         if (roles.contains(Role.ADMIN)) {
             hasProfile = true;
+
         } else if (roles.contains(Role.SEEKER)) {
             hasProfile = jobSeekerRepository.findByUserId(user.getId()).isPresent();
         } else if (roles.contains(Role.EMPLOYER)) {
