4e473586b963a5d22180ece104ae9e6d4ed2fd95
Taboni986 <donthuan090806@gmail.com>
2026-06-14 11:53:06 +0700

    fix: plan name

--- src/main/java/Cloudian/JobPortal/modules/payment/PlanRepository.java
+++ src/main/java/Cloudian/JobPortal/modules/payment/PlanRepository.java
@@ -6,4 +6,6 @@ import java.util.Optional;
 
 public interface PlanRepository extends JpaRepository<Plan, Long> {
     Optional<Plan> findByName(String name);
+
+    Optional<Plan> findByNameIgnoreCase(String name);
 }
--- src/main/java/Cloudian/JobPortal/modules/payment/dto/CreatePaymentDto.java
+++ src/main/java/Cloudian/JobPortal/modules/payment/dto/CreatePaymentDto.java
@@ -14,8 +14,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class CreatePaymentDto {
-    private Long planId;
-
     @NotBlank(message = "Plan name is required")
     private String planName;
     
